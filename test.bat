626e25625b623e3a79efb3dd4b676aa84d467d0d
Yuki Kurosawa <yuki@gakkougurashi.net>
2022-02-08 13:49:45 +0800

    Fix Standard-Json Compile to FileSystem (not stdin)

--- test.bat
+++ test.bat
@@ -1,3 +1,4 @@
 @echo off
 rd /s /q out
-node yukicli ..\erc20-tokens\contracts\YukiNetworkToken.sol --bin --abi -o out --base-path ..\erc20-tokens\contracts --include-path ..\erc20-tokens\node_modules
+node yukicli ..\erc20-tokens\contracts\YukiNetworkToken.sol --bin --abi --pretty-json -o out --base-path ..\erc20-tokens\contracts --include-path ..\erc20-tokens\node_modules
+node yukicli --standard-json --abi --bin --pretty-json out\StandardJSON.json> out\out.json
